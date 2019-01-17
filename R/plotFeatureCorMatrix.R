43786fc05bf3740b5feacbaa54d2a83991e23e57
afmoebius1 <afmoebius1@gmail.com>
2019-01-17 21:58:20 +0100

    new plot function

--- R/plotFeatureCorMatrix.R
+++ R/plotFeatureCorMatrix.R
@@ -10,5 +10,16 @@
 #' @examples
 #'
 plotFeatureCorMatrix <- function(listCorMatrix) {
-  heatmap(listCorMatrix[[1]], Rowv = NA, Colv = "Rowv", scale = "none")
+  # transforms matrix to data.frame
+  dt_corMatrix <- data.table::melt(result[[1]])
+  # plot columns of data.frame
+  ggplot2::ggplot(dt_corMatrix, ggplot2::aes(Var1, Var2, value)) +
+    # plot rectangles
+    ggplot2::geom_tile(ggplot2::aes(fill = value)) +
+    # no background and plot boarder
+    ggplot2::theme_bw() +
+    # use nice colors
+    ggplot2::scale_fill_gradient(low="white", high="darkgreen") +
+    # start in the up left cornor (by reversing the y-axis)
+    ggplot2::scale_y_discrete(limits = rev(levels(dt_corMatrix$Var2)))
 }
